9ea75912bfa2e53dce7aa0bc0e827aad856872b6
bikashdas0108 <bikashdas0108@gmail.com>
2025-07-07 19:44:34 +0530

    fix: fix no data img

--- src/BaseTable/components/NoDataError/index.tsx
+++ src/BaseTable/components/NoDataError/index.tsx
@@ -1,9 +1,8 @@
-import { getCdnUrl } from '../../../utils/general';
-
+import catImg from '../../../assets/images/cat.svg';
 const NoDataError = ({ text = 'Looks like there is no data to show' }) => {
   return (
     <div className='w-full h-full flex flex-col justify-center items-center p-6'>
-      <img className='mb-4' width='95' src={getCdnUrl('/images/cat.svg')} alt='cat' />
+      <img className='mb-4' width='95' src={catImg} alt='cat' />
       <div className='text-base text-[#000000] font-medium'>{text}</div>
     </div>
   );
--- src/utils/general.ts
+++
@@ -1,3 +0,0 @@
-export const getCdnUrl = (url: string): string => {
-  return process.env.REACT_APP_ASSET_BASE_URL + url;
-};
